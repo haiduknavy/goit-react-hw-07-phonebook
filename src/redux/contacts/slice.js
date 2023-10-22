c4181caeccba08203288ed4b7d0c175f62b88a23
haiduknavy <79574457+haiduknavy@users.noreply.github.com>
2023-10-22 16:14:45 +0300

    End HW

---
+++ src/redux/contacts/slice.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from 'redux/operations';
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const contactsState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
+export const contactSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsState,
+  reducers: {},
+  extraReducers: builder =>
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(addContact.rejected, handleRejected)
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = state.items.filter(
+          contact => contact.id !== action.payload
+        );
+      })
+      .addCase(deleteContact.rejected, handleRejected),
+});
+
+export const contactsReducer = contactSlice.reducer;
